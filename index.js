5eaf10b321acffb0232c617b792a73446122549a
patch <46700102+Patchwtf@users.noreply.github.com>
2023-01-25 17:58:08 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
   $(window).scroll(function() {
     var barra = $(window).scrollTop();
-    var posicion = barra * 0.09;
+    var posicion = barra * 0.1;
     var p2 = barra * 0.7;
     var op = barra * 0.003;
 
     $('body').css({
       'background-position': '0 -' + posicion + 'px'
     });
-    $('.fondoZ2').css({
+    $('.work-gallery').css({
       'background-position': '0 ' + p2 + 'px'
     });
     $('h2').css({
@@ -21,7 +21,7 @@ $(document).ready(function() {
       'top': p2+'px'
     });
     $('.bg-up-one').css({
-      'background-position': '0 +' + (barra * 0.5)+'px'
+      'background-position': '0 -' + (barra * 0.9)+'px'
     });
     
     console.log(barra);
