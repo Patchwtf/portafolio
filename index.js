fb6cab6dcab0e3c64cb30aca4376ec9686af5cf0
“Darko” <jhosbyndaniel@gmail.com>
2023-01-26 16:02:09 -0600

    Se termino todo el respondive design. Faltan los links

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
-$(document).ready(function() {
-  $(window).scroll(function() {
+$(document).ready(function () {
+  $(window).scroll(function () {
     var barra = $(window).scrollTop();
-    var posicion = barra * 0.1;
+    var posicion;
     var p2 = barra * 0.7;
     var op = barra * 0.003;
 
-    $('body').css({
-      'background-position': '0 -' + posicion + 'px'
+    var resolution = screen.availWidth;
+
+    if (resolution > 768) {
+      posicion = barra * 0.1;
+    } else {
+      posicion = barra * 0.09;
+    }
+
+    $("body").css({
+      "background-position": "0 -" + posicion + "px",
     });
-    $('.work-gallery').css({
-      'background-position': '0 ' + p2 + 'px'
+    $(".work-gallery").css({
+      "background-position": "0 " + p2 + "px",
     });
-    $('h2').css({
-      'top': '-' + p2 + 'px'
+    $("h2").css({
+      top: "-" + p2 + "px",
     });
-    $('section.descrive-me .text-me').css({
-      'opacity': op
+    $("section.descrive-me .text-me").css({
+      opacity: op,
     });
-    $('section.descrive-me .photo-icon').css({
-      'top': p2+'px'
+    $("section.descrive-me .photo-icon").css({
+      top: p2 + "px",
     });
-    $('.bg-up-one').css({
-      'background-position': '0 -' + (barra * 0.9)+'px'
+    $(".bg-up-one").css({
+      "background-position": "0 -" + barra * 0.9 + "px",
     });
-    
-    console.log(barra);
   });
 });
