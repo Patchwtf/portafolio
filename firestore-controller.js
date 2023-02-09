666b29113268c0862b977d2f74d159667ebedc57
“Darko” <jhosbyndaniel@gmail.com>
2023-02-09 01:37:27 -0600

    Proyecto terminado con formulario conectado

--- firestore-controller.js
+++ firestore-controller.js
@@ -1,3 +1,20 @@
-const contactForm = document.querySelector("#contact-form");
+const db = firebase.firestore();
 
-contactForm.addEventListener('submit', e => { console.log("Hola Mundo") })
+const contactForm = document.querySelector(".formulario");
+
+contactForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const name = contactForm["user-name"].value;
+  const phone = contactForm["user-phone"].value;
+  const mail = contactForm["user-mail"].value;
+  const message = contactForm["user-message"].value;
+  var today = new Date();
+  var now = today.toLocaleString();
+
+  if (name != "" && phone != "" && mail != "" && message != "") {
+    db.collection("clientes").doc().set({ name, phone, mail, message, now });
+    contactForm.reset();
+  } else {
+    alert("No se pueden enviar los datos si algun campo esta vacío");
+  }
+});
